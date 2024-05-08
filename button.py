6e55ca934f567271955ca5851faf87f4c2ca2c9f
muhaimin <12211068100@student.mmu.edu.my>
2024-05-08 16:27:17 +0800

    click sound 2

--- button.py
+++ button.py
@@ -14,7 +14,7 @@ class Button():
 		self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
 		self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
 		#self.hover_sound = pygame.mixer.Sound()
-		s#elf.click_sound = pygame.mixer.Sound()
+		#elf.click_sound = pygame.mixer.Sound()
 
 	def update(self, screen):
 		if self.image is not None:
